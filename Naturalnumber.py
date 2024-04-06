a407937f2ea08aa47e4d9254936c4efa8f6f7219
Hafeezco75 <hafeezco75@gmail.com>
2024-04-06 02:19:35 -0700

    adding more assignments

---
+++ Naturalnumber.py
@@ -0,0 +1,16 @@
+naturalNum = 0-10;
+
+natNum1 = int(input("Enter first natural number "))
+natNum2 = int(input("Enter second natural number "))
+natNum3 = int(input("Enter third natural number "))
+natNum4	= int(input("Enter first natural number "))
+natNum5 = int(input("Enter first natural number "))
+natNum6 = int(input("Enter first natural number "))
+natNum7 = int(input("Enter first natural number "))
+natNum8 = int(input("Enter first natural number "))
+natNum9 = int(input("Enter first natural number "))
+natNum10 = int(input("Enter first natural number "))
+
+sum = natNum1 + natNum2 + natNum3 + natNum4 + natNum5 + natNum6 + natNum7 + natNum8 + natNum9 + natNum10
+
+print(f'The sum of Natural Number is {sum}')
