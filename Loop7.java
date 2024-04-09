2f4c30962babbd633b7fa18207d62aa5502ba94b
Hafeezco75 <hafeezco75@gmail.com>
2024-04-09 07:47:29 -0700

    additional files for Loop

---
+++ Loop7.java
@@ -0,0 +1,12 @@
+public class Loop7 {
+  public static void main(String[] args) {
+
+   System.out.println("5 4 3 2 1");
+   System.out.println("4 3 2 1");
+   System.out.println("3 2 1");
+   System.out.println("2 1");
+   System.out.println("1");
+
+}
+
+}
