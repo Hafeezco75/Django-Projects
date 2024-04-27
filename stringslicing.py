f59e3a8d3e6901700f13ba3f10a1a61709c88294
Hafeezco75 <hafeezco75@gmail.com>
2024-04-27 10:53:50 -0700

    Adding function and test files to git repository

---
+++ stringslicing.py
@@ -0,0 +1,16 @@
+def length(string):
+
+	len(string) 
+
+	return len(string[:2]) + len(string[2:])
+
+	if len(string) == 2:
+
+		return len(string) + len(string[:2])
+
+	empty_string = ""
+
+	if len(string) < 2:
+
+		return empty_string
+	 
